a1f777c3d79a5b125946f0ec9612de090abce1ea
Danilo <danilogmoura@gmail.com>
2024-03-11 05:31:42 -0300

    Renomeando Script

--- SonarQube-Install.ps1
+++ SonarQube-Install.ps1
@@ -37,12 +37,13 @@ wsl --import $distroName $distroPath $alpineFilePath
 Remove-Item $alpineFilePath
 
 # Testar a instalação
-wsl -d $distroName -- echo "Alpine Linux $version $architecture instalado com sucesso!"
+wsl -d $distroName -- echo "`r`nAlpine Linux $version $architecture instalado com sucesso!"
 
 # Instalando o Docker
+wsl -d $distroName -- sysctl -w vm.max_map_count=262144
+
 wsl -d $distroName -- apk update 
 wsl -d $distroName -- apk upgrade 
-wsl -d $distroName -- apk add docker py3-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make 
 
 wsl -d $distroName -- apk add docker
 Start-Sleep -Seconds 5
@@ -60,15 +61,15 @@ try {
     $serverCheck = $dockerVersionLines -match "Server:"
 
     if (-not $clientCheck) {
-        Write-Host "O cliente Docker não iniciou corretamente." -ForegroundColor Red
+        Write-Host "`r`nO cliente Docker não iniciou corretamente." -ForegroundColor Red
         exit
     }
     elseif (-not $serverCheck) {
-        Write-Host "O serviço Docker não subiu corretamente." -ForegroundColor Red
+        Write-Host "`r`nO serviço Docker não subiu corretamente." -ForegroundColor Red
         exit
     }
     else {
-        Write-Host "Tanto o cliente quanto o serviço Docker estão funcionando corretamente." -ForegroundColor Green
+        Write-Host "`r`nTanto o cliente quanto o serviço Docker estão funcionando corretamente." -ForegroundColor Green
         # Exibindo a versão do cliente e do servidor
         $clientVersion = $dockerVersionLines | Where-Object { $_ -match "Version:.*" } | Select-Object -First 1
         $serverVersion = $dockerVersionLines | Where-Object { $_ -match "Version:.*" } | Select-Object -Last 1
@@ -77,10 +78,54 @@ try {
     }
 }
 catch {
-    Write-Host "Ocorreu um erro ao tentar executar o comando 'docker version' no WSL." -ForegroundColor Red
+    Write-Host "`r`nOcorreu um erro ao tentar executar o comando 'docker version' no WSL." -ForegroundColor Red
     Write-Host "Erro: $_" -ForegroundColor Red
 }
 
 # Instalando o Docker Compose
 wsl -d $distroName -- apk add docker docker-cli-compose
 
+# Copia o arquivo docker-compose.yml para a pasta /root do Alpine
+wsl -d $distroName -- cp ./compose/docker-compose.yml /root/docker-compose.yml
+
+# wsl -d $distroName -- 
+wsl -d $distroName -- ls /root
+
+# Iniciando o SonarQube com o Docker Compose
+wsl -d sonarqube -- docker-compose -f /root/docker-compose.yml up -d
+
+# Simulação da saída do comando, substitua essa parte pela captura real do seu comando docker-compose
+$output = @"
+[+] Running 7/7
+ ✔ Network root_default                Created
+ ✔ Volume "root_sonarqube_db"          Created
+ ✔ Volume "root_sonarqube_data"        Created
+ ✔ Volume "root_sonarqube_extensions"  Created
+ ✔ Volume "root_sonarqube_logs"        Created
+ ✔ Container sonarqube_db              Started
+ ✔ Container sonarqube                 Started
+"@
+
+# Separa as linhas e verifica apenas as últimas 7
+$lines = $output -split "`n" | Select-Object -Last 7
+
+# Variável para manter o status de sucesso
+$allSuccessful = $true
+
+foreach ($line in $lines) {
+    if (-not($line -match "✔")) {
+        $allSuccessful = $false
+        Write-Host "`r`nFalha detectada na linha: $line" -ForegroundColor Red
+    }
+}
+
+if ($allSuccessful) {
+    Write-Host "`r`nTodos os serviços (networks, volumes, containers) foram criados ou iniciados com sucesso." -ForegroundColor Green
+}
+else {
+    Write-Host "`r`nAlguns serviços não foram iniciados corretamente." -ForegroundColor Red
+    exit
+}
+
+
+Write-Host "`r`nInstalação concluida, o serviço está ON, quando precisar reiniciar user o script 'SonarQube-Start.ps1' " -ForegroundColor Green
