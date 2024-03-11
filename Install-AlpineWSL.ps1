f8eb8d8e51ea185c2571a6e401937ee25ab321ce
Danilo <danilogmoura@gmail.com>
2024-03-11 01:39:39 -0300

    Instalação do docker e compose no Alpine

--- Install-AlpineWSL.ps1
+++ Install-AlpineWSL.ps1
@@ -35,3 +35,52 @@ wsl --import $distroName $distroPath $alpineFilePath
 
 # Remover o arquivo tar.gz após a importação
 Remove-Item $alpineFilePath
+
+# Testar a instalação
+wsl -d $distroName -- echo "Alpine Linux $version $architecture instalado com sucesso!"
+
+# Instalando o Docker
+wsl -d $distroName -- apk update 
+wsl -d $distroName -- apk upgrade 
+wsl -d $distroName -- apk add docker py3-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make 
+
+wsl -d $distroName -- apk add docker
+Start-Sleep -Seconds 5
+
+wsl -d $distroName -- dockerd &
+Start-Sleep -Seconds 5
+
+# Executa o comando 'docker version' no WSL e captura a saída
+try {
+    $dockerVersionOutput = wsl -d $distroName -- docker version
+    
+    # Convertendo a saída em uma lista para facilitar a busca
+    $dockerVersionLines = $dockerVersionOutput -split "`n"
+    $clientCheck = $dockerVersionLines -match "Client:"
+    $serverCheck = $dockerVersionLines -match "Server:"
+
+    if (-not $clientCheck) {
+        Write-Host "O cliente Docker não iniciou corretamente." -ForegroundColor Red
+        exit
+    }
+    elseif (-not $serverCheck) {
+        Write-Host "O serviço Docker não subiu corretamente." -ForegroundColor Red
+        exit
+    }
+    else {
+        Write-Host "Tanto o cliente quanto o serviço Docker estão funcionando corretamente." -ForegroundColor Green
+        # Exibindo a versão do cliente e do servidor
+        $clientVersion = $dockerVersionLines | Where-Object { $_ -match "Version:.*" } | Select-Object -First 1
+        $serverVersion = $dockerVersionLines | Where-Object { $_ -match "Version:.*" } | Select-Object -Last 1
+        Write-Host "Versão do Cliente: $clientVersion" -ForegroundColor Green
+        Write-Host "Versão do Servidor: $serverVersion" -ForegroundColor Green
+    }
+}
+catch {
+    Write-Host "Ocorreu um erro ao tentar executar o comando 'docker version' no WSL." -ForegroundColor Red
+    Write-Host "Erro: $_" -ForegroundColor Red
+}
+
+# Instalando o Docker Compose
+wsl -d $distroName -- apk add docker docker-cli-compose
+
